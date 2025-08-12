8a4b10b6937060da82aa20b466c614995eaf95b9
Pahmat880 <amay58909@gmail.com>
2025-08-12 20:49:15 +0700

    Add files via upload

--- api/submit-form.js
+++ api/submit-form.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
 
         const mailOptions = {
             from: `"Formulir Pemesanan" <${process.env.EMAIL_USER}>`,
-            to: process.env.EMAIL_USER,
+            to: ahmatptt@gmail.com,
             subject: `Pesanan Baru #${kodeOrder}`,
             text: emailText
         };
